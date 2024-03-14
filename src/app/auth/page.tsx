4e2841f96d4c4118d1869394b4458969784d9a50
YASH GHARAT <53621256+Yash8452@users.noreply.github.com>
2024-03-14 14:41:04 +0000

    initial commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AUTH() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between pt-4 p-24">
+            <h1>LOGIN/SIGNUP</h1>
+        </main>
+    )
+}
+
+export default AUTH
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between pt-4 p-24">
+      <h1>DASHBOARD</h1>
+    </main>
+  );
+}
